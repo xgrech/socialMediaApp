d7e1dac10198ae3d05d67a0383d331082b2f3899
erikg <erikgrech@gmail.com>
2018-12-06 20:02:34 +0100

    Firebase Setup - Auth - Crashlytics

--- app/src/main/java/gmd/socialmediaapp/MainActivity.java
+++ app/src/main/java/gmd/socialmediaapp/MainActivity.java
@@ -1,13 +1,84 @@
 package gmd.socialmediaapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.crashlytics.android.Crashlytics;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    public static final int RC_SIGN_IN = 1;
+    private FirebaseAuth.AuthStateListener mAuthStateListner;
+
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAuth=FirebaseAuth.getInstance();
+        mAuthStateListner=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if (user!=null)
+                {
+                    Toast.makeText(MainActivity.this, "User Signed In", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(providers)
+                                    .build(),
+                            RC_SIGN_IN
+                    );
+
+                }
+            }
+        };
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListner);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListner);
+
+    }
+
+
+    public void signout(View view) {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MainActivity.this, "User Signed Out", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
     }
 }
